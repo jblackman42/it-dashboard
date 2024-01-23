f3ca428ac3a5db0bbd31fd13274e73828c6a4683
jblackman42 <jblackman@pureheart.org>
2024-01-23 10:57:40 -0700

    added user dependency

--- src/pages/Ha.jsx
+++ src/pages/Ha.jsx
@@ -1,67 +1,68 @@
 import React, { useContext, useState, useEffect } from "react";
 import axios from "axios";
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
 
-import { LoadingContext } from '../auth/ProtectedRoute';
+import { LoadingContext } from "../auth/ProtectedRoute";
 import { getToken } from "../auth";
 
 const getHaInformation = async () => {
-    // console.log(onlyMyTickets)
-    return await axios({
-      method: "post",
-      url: "https://my.pureheart.org/ministryplatformapi/procs/PHCGetHaMinistryQuestionInfo",
-      data: {
-        "@DaysBack": 100
-      },
-      headers: {
-        "Authorization": `Bearer ${await getToken()}`,
-        "Content-Type": "Application/JSON"
-      }
-    })
-    .then(response => response.data)
-  }
+  // console.log(onlyMyTickets)
+  return await axios({
+    method: "post",
+    url: "https://my.pureheart.org/ministryplatformapi/procs/PHCGetHaMinistryQuestionInfo",
+    data: {
+      "@DaysBack": 100,
+    },
+    headers: {
+      Authorization: `Bearer ${await getToken()}`,
+      "Content-Type": "Application/JSON",
+    },
+  }).then((response) => response.data);
+};
 
 const Ha = () => {
-    // const daysBack = 365
-    const [questions, setQuestions] = useState([]);
-    const [weekData, setWeekData] = useState([]);
-    const [monthData, setMonthData] = useState([]);
-    const [user, setUser] = useState({});
- 
-    const { startLoading, stopLoading } = useContext(LoadingContext);
- 
-    useEffect(() => {
-      if (Cookies.get("user")) setUser(JSON.parse(Cookies.get("user")));
-    }, []);
- 
-    useEffect(() => {
-        if (user.userid) {
-            startLoading();
-          getHaInformation().then(data => {
-            const [questions, weekData, monthData] = data;
-            setQuestions(questions);
-            setWeekData(weekData);
-            setMonthData(monthData);
-            
-            stopLoading();
-          }).catch(err => {
-            console.log(err); //remove this in production but it's usefull for now
-            stopLoading();
-          })
-        };
-    }, [startLoading, stopLoading]);
+  // const daysBack = 365
+  const [questions, setQuestions] = useState([]);
+  const [weekData, setWeekData] = useState([]);
+  const [monthData, setMonthData] = useState([]);
+  const [user, setUser] = useState({});
 
-    return (
-        <div>
-            <h1>Hello World!</h1>
-            {questions.map((question, index) => (
-                <div key={index}>
-                    <h2>{question.Question_Title}</h2>
-                    <p>{question.Question_Description}</p>
-                </div>
-            ))}
+  const { startLoading, stopLoading } = useContext(LoadingContext);
+
+  useEffect(() => {
+    if (Cookies.get("user")) setUser(JSON.parse(Cookies.get("user")));
+  }, []);
+
+  useEffect(() => {
+    if (user.userid) {
+      startLoading();
+      getHaInformation()
+        .then((data) => {
+          const [questions, weekData, monthData] = data;
+          setQuestions(questions);
+          setWeekData(weekData);
+          setMonthData(monthData);
+
+          stopLoading();
+        })
+        .catch((err) => {
+          console.log(err); //remove this in production but it's usefull for now
+          stopLoading();
+        });
+    }
+  }, [startLoading, stopLoading, user]);
+
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      {questions.map((question, index) => (
+        <div key={index}>
+          <h2>{question.Question_Title}</h2>
+          <p>{question.Question_Description}</p>
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 
-export default Ha
+export default Ha;
